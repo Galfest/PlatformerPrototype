175afe04fbf573ca3334173badfe54a35d506eec
Galfest <abramyants4@gmail.com>
2023-01-30 18:01:22 +0300

    Controls

--- App/src/main/java/main/GamePanel.java
+++ App/src/main/java/main/GamePanel.java
@@ -12,6 +12,10 @@ public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
     private int xDelta = 100, yDelta = 100;
+    private int xDir = 1, yDir = 1;
+    private int frames = 0;
+    private long lastCheck = 0;
+
 
     public GamePanel (){
 
@@ -24,24 +28,47 @@ public class GamePanel extends JPanel {
 
     public void changeXDelta(int value){
         this.xDelta += value;
-        repaint();
+
     }
 
     public void changeYDelta(int value){
         this.yDelta += value;
-        repaint();
+
     }
 
     public void setRectPos(int x, int y){
         this.xDelta = x;
         this.yDelta = y;
-        repaint();
+
     }
 
+    //Вызывает repaint() и перерисовывает изменения после ввода.
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-
+        updateRectangle();
         g.fillRect(xDelta, yDelta,200,50);
 
+        frames++;
+        if (System.currentTimeMillis() - lastCheck >= 1000){
+            lastCheck = System.currentTimeMillis();
+            System.out.println("FPS " + frames);
+            frames = 0;
+
+        }
+
+
+        repaint();
+
+    }
+
+    private void updateRectangle() {
+        xDelta += xDir;
+        if (xDelta > 400 || xDelta < 0){
+            xDir*= -1;
+        }
+        yDelta += yDir;
+        if (yDelta > 400 || yDelta < 0){
+            yDir *= -1;
+        }
     }
 }
--- App/src/main/java/main/GameWindow.java
+++ App/src/main/java/main/GameWindow.java
@@ -7,7 +7,7 @@ public class GameWindow {
     public GameWindow(GamePanel gamePanel){
         jFrame = new JFrame();
 
-        jFrame.setSize(800,800);
+        jFrame.setSize(400,400);
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         jFrame.add(gamePanel);
         jFrame.setLocationRelativeTo(null);
