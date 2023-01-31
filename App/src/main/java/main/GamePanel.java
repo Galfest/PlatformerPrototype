bc84604dd0ff2dd39478f6860000ae8be0ee79f5
Galfest <abramyants4@gmail.com>
2023-01-31 17:46:20 +0300

    V2.1

--- App/src/main/java/main/GamePanel.java
+++ App/src/main/java/main/GamePanel.java
@@ -7,18 +7,21 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
 
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
-    private int xDelta = 100, yDelta = 100;
-    private int xDir = 1, yDir = 1;
+    private float xDelta = 100, yDelta = 100;
+    private float xDir = 1f, yDir = 1f;
     private int frames = 0;
     private long lastCheck = 0;
+    private Color color = new Color(150,20,90);
+    private Random random;
 
 
     public GamePanel (){
-
+        random = new Random();
         mouseInputs = new MouseInputs(this);
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
@@ -46,18 +49,10 @@ public class GamePanel extends JPanel {
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         updateRectangle();
-        g.fillRect(xDelta, yDelta,200,50);
+        g.setColor(color);
+        g.fillRect((int) xDelta, (int) yDelta,50,50);
 
-        frames++;
-        if (System.currentTimeMillis() - lastCheck >= 1000){
-            lastCheck = System.currentTimeMillis();
-            System.out.println("FPS " + frames);
-            frames = 0;
-
-        }
-
-
-        repaint();
+        //Счетчик FPS в консоль.
 
     }
 
@@ -65,10 +60,20 @@ public class GamePanel extends JPanel {
         xDelta += xDir;
         if (xDelta > 400 || xDelta < 0){
             xDir*= -1;
+            color = getRndColor();
         }
         yDelta += yDir;
         if (yDelta > 400 || yDelta < 0){
             yDir *= -1;
+            color = getRndColor();
         }
+    }
+
+    private Color getRndColor(){
+        int r = random.nextInt(255);
+        int b = random.nextInt(255);;
+        int g = random.nextInt(255);;
+
+        return new Color(r,b,g);
     }
 }
--- App/src/main/java/main/GameWindow.java
+++ App/src/main/java/main/GameWindow.java
@@ -8,6 +8,7 @@ public class GameWindow {
         jFrame = new JFrame();
 
         jFrame.setSize(400,400);
+        jFrame.setTitle("Seven kings");
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         jFrame.add(gamePanel);
         jFrame.setLocationRelativeTo(null);
